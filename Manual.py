3479880dde769e6ae43ab22d1839ce5d6dd75942
Mugta1 <mayankgupta_23bt043@dtu.ac.in>
2024-06-20 21:00:17 +0530

    Added Error Handling

--- Manual.py
+++ Manual.py
@@ -24,23 +24,32 @@ def rename(media):
         #Take input for the new name from user
         print(f'The name of this file is {element[1]}')
         nname=str(input("Enter the new name for this file"))
-        newname= os.path.join(element[0], nname)
-        file=os.path.join(element[0], element[1])
-        #Rename
-        os.rename(file, newname)
-
+        try:
+            newname= os.path.join(element[0], nname)
+            file=os.path.join(element[0], element[1])
+            #Rename
+            os.rename(file, newname)
+        except:
+            print(f"Could not rename file {element[1]}")
 #Deletes obsolete files other than the files with extensions specified in the subtitle_formats and video_formats tuples.
-def deleter(directory):     
+def deleter(directory):
+    count=0
     for path, subdir, files in os.walk(directory):
          for file in files:
             if not file.lower().endswith(subtitle_formats) and not file.lower().endswith(video_formats):
-                os.remove(os.path.join(path,file))
+                try:
+                    os.remove(os.path.join(path,file))
+                    count+=1
+                except:
+                    print(f"Could not delete file {file}")
+
 def choice():
     choice= input("Do you want to rename any other directory? \n 1. Yes \n 2.No")
     if choice.lower()=='yes' or choice=='1':
         main()
     elif choice.lower()=='no' or choice =='2':
         print("Thank you for using this program :)")
+        return
     else:
         print("Wrong input, try again")
         choice()
@@ -49,9 +58,16 @@ def choice():
 def main():
     print("Welcome to media renamer ^-^")
     directory=str(input("please enter the directory to be renamed"))
+    if not os.path.isdir(directory):
+        print("Wrong Directory, exiting")
+        choice()
+    
     #format=str(input("Please enter the format you will like the files to be renamed in"))
     media=findingmedia(directory)
-
+    if not media:
+        print(f"No media files in this directory \n {directory}")
+        choice()
+    
     rename(media)
     delete= input("Do you want to delete obsolete files in this directory? \n 1. Yes \n 2.No")
     if delete.lower()=='yes' or delete=='1':

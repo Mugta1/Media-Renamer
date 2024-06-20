7f373958e8e1bf360dc7aef7fb1d061d82175bf0
Mugta1 <mayankgupta_23bt043@dtu.ac.in>
2024-06-20 20:02:55 +0530

    one-third done.

--- script.py
+++ script.py
@@ -1,3 +1,35 @@
-import sys
+
+import os
+import openai
 #Initial commit
-# i dont want to lose this streak help
+
+openai.api_keyapi= "your openai key"
+
+#get the name of media files in a given directory
+
+def findingmedia(directory):
+    media=[]
+    for path,subdir, files in os.walk(directory):
+        for file in files:
+            if file.lower.endswith(('.mp4', '.avi', '.mkv', '.mov', '.wmv', '.flv', '.m4v', '.mpg', '.mpeg', '.3gp', '.3g2', '.webm', '.vob', '.ogv', '.ts')):
+                media.append(os.path.join(path,file))
+
+    return media
+
+
+
+
+#get the format from the user and get the names of files from openAI according to that
+
+
+    
+
+#Rename
+
+
+
+#final function
+
+def main():
+    directory=str(input("please enter the directory to be renamed"))
+    format=str(input("Please enter the format you will like the files to be renamed in"))

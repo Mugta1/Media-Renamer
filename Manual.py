4b7f72b0da898aff8fd7f3b65114adbf5fa7d552
Mugta1 <mayankgupta_23bt043@dtu.ac.in>
2024-06-20 20:24:22 +0530

    First Function Defined

--- Manual.py
+++ Manual.py
@@ -1,18 +1,29 @@
 ##This can be used to rename your files manually, without using OpenAI or Any other LLM
 import os
 
-#Initial commit
 
+
+
+#Add/Remove extensions from these lists according to your need.
+video_formats= ('.mp4', '.avi', '.mkv', '.mov', '.wmv', '.flv', '.m4v', '.mpg', '.mpeg', '.3gp', '.3g2', '.webm', '.vob', '.ogv', '.ts')
+subtitle_formats= ('.srt', '.vtt', '.sbv', '.sub', '.ass/.ssa', '.dfxp')
 
 
 #get the name of media files in a given directory
-
 def findingmedia(directory):
     media=[]
     for path,subdir, files in os.walk(directory):
         for file in files:
-            if file.lower.endswith(('.mp4', '.avi', '.mkv', '.mov', '.wmv', '.flv', '.m4v', '.mpg', '.mpeg', '.3gp', '.3g2', '.webm', '.vob', '.ogv', '.ts')):
+            #Appends the path and names of video files into media list.
+            if file.lower.endswith(video_formats):
                 media.append(os.path.join(path,file))
+            #Checks if other files are subtitle files, if not, deletes them.
+            elif not file.lower.endswith(subtitle_formats):
+                os.remove(file)
+
+
+
+            
 
     return media
 
@@ -23,7 +34,8 @@ def findingmedia(directory):
 
 
 #get the format from the user and get the names of files from openAI according to that
-def rename(media, :
+def rename(media):
+
 
 
 
